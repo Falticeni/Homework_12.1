25e7cfebdf429475ce9be1885d28e40d0a45fd17
Ciocan Robert Vasile <ciocanrobertvasile91@gmail.com>
2019-07-23 15:50:58 +0300

    Homework 13.2

---
+++ MyMVCApplication/Controllers/Http_Helpers_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApplication.Controllers
+{
+    public class Http_Helpers_Controller : Controller
+    {
+        // GET: Http_Helpers_
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
